b7345f752aa350769b5afe3a3d2f70c59a62785b
Kojo Kwakye <kodeon6@gmail.com>
2020-01-15 22:41:03 +0000

    Learning

---
+++ index.js
@@ -0,0 +1,98 @@
+var content1 = document.getElementById('content1')
+var content2 = document.getElementById('content2')
+var content3 = document.getElementById('content3')
+var content4 = document.getElementById('content4')
+var content5 = document.getElementById('content5')
+
+var btn1 = document.getElementById('btn1')
+var btn2 = document.getElementById('btn2')
+var btn3 = document.getElementById('btn3')
+var btn4 = document.getElementById('btn4')
+var btn5 = document.getElementById('btn5')
+
+
+function openHTML() {
+  content1.style.transform = 'translateX(0)'
+  content2.style.transform = 'translateX(100%)'
+  content3.style.transform = 'translateX(100%)'
+  content4.style.transform = 'translateX(100%)'
+  content5.style.transform = 'translateX(100%)'
+  btn1.style.color = "#612385";
+  btn2.style.color = "#000000";
+  btn3.style.color = "#000000";
+  btn4.style.color = "#000000";
+  btn5.style.color = "#000000";
+  content1.style.transitionDelay = ".3s"
+  content2.style.transitionDelay = ".0s"
+  content3.style.transitionDelay = ".0s"
+  content4.style.transitionDelay = ".0s"
+  content5.style.transitionDelay = ".0s"
+}
+function openCSS() {
+  content1.style.transform = 'translateX(100%)'
+  content2.style.transform = 'translateX(0)'
+  content3.style.transform = 'translateX(100%)'
+  content4.style.transform = 'translateX(100%)'
+  content5.style.transform = 'translateX(100%)'
+  btn1.style.color = "#000000";
+  btn2.style.color = "#612385";
+  btn3.style.color = "#000000";
+  btn4.style.color = "#000000";
+  btn5.style.color = "#000000";
+  content1.style.transitionDelay = ".0s"
+  content2.style.transitionDelay = ".3s"
+  content3.style.transitionDelay = ".0s"
+  content4.style.transitionDelay = ".0s"
+  content5.style.transitionDelay = ".0s"
+}
+function openJS() {
+  content1.style.transform = 'translateX(100%)'
+  content2.style.transform = 'translateX(100%)'
+  content3.style.transform = 'translateX(0)'
+  content4.style.transform = 'translateX(100%)'
+  content5.style.transform = 'translateX(100%)'
+  btn1.style.color = "#000000";
+  btn2.style.color = "#000000";
+  btn3.style.color = "#612385";
+  btn4.style.color = "#000000";
+  btn5.style.color = "#000000";
+  content1.style.transitionDelay = ".0s"
+  content2.style.transitionDelay = ".0s"
+  content3.style.transitionDelay = ".3s"
+  content4.style.transitionDelay = ".0s"
+  content5.style.transitionDelay = ".0s"
+}
+function openPY() {
+  content1.style.transform = 'translateX(100%)'
+  content2.style.transform = 'translateX(100%)'
+  content3.style.transform = 'translateX(100%)'
+  content4.style.transform = 'translateX(0%)'
+  content5.style.transform = 'translateX(100%)'
+  btn1.style.color = "#000000";
+  btn2.style.color = "#000000";
+  btn3.style.color = "#000000";
+  btn4.style.color = "#612385";
+  btn5.style.color = "#000000";
+  content1.style.transitionDelay = ".0s"
+  content2.style.transitionDelay = ".0s"
+  content3.style.transitionDelay = ".0s"
+  content4.style.transitionDelay = ".3s"
+  content5.style.transitionDelay = ".0s"
+}
+function openCD() {
+  content1.style.transform = 'translateX(100%)'
+  content2.style.transform = 'translateX(100%)'
+  content3.style.transform = 'translateX(100%)'
+  content4.style.transform = 'translateX(100%)'
+  content5.style.transform = 'translateX(0%)'
+  btn1.style.color = "#000000";
+  btn2.style.color = "#000000";
+  btn3.style.color = "#000000";
+  btn4.style.color = "#000000";
+  btn5.style.color = "#612385";
+  content1.style.transitionDelay = ".0s"
+  content2.style.transitionDelay = ".0s"
+  content3.style.transitionDelay = ".0s"
+  content4.style.transitionDelay = ".0s"
+  content5.style.transitionDelay = ".3s"
+}
